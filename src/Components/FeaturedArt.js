5d039c3a521967d31af9d3214095c7ff8fc62ab9
lalsa chourasia <lalsachourasia0512.com>
2022-01-21 15:16:58 +0530

    done

--- src/Components/FeaturedArt.js
+++ src/Components/FeaturedArt.js
@@ -3,14 +3,12 @@ import { useContext } from 'react/cjs/react.development'
 import {NoteContext} from '../Components/Category/NoteContext'
 
 const FeaturedArt = () => {
-const [data, setData] = useContext(NoteContext);
+// const [data, setData] = useContext(NoteContext);
 
-const hndl=()=>{
-           console.log(data[0].title);
-           setData=()=>{
-               console.log("hello");
-           }
-    }
+// const hndl=()=>{
+//            console.log(data[0].title);
+         
+//     }
     return (
         <div>
             <>
@@ -19,7 +17,8 @@ const hndl=()=>{
         </div>
         <div className='featured_art2'>
             <div className='small_1'>Title: {data[0].title}</div>
-            <div className='small_2' onClick={hndl}></div>
+            <div className='small_2'></div>
+            {/* <div className='small_2' onClick={hndl}></div> */}
         </div>
         </>
         </div>
